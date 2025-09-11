4cb1fd0c354199f7d93ae62b9e87fa75b282758f
hotdogman777 <hhc0614@naver.com>
2025-09-11 12:52:58 +0900

    수업 나감

---
+++ 0911.py
@@ -0,0 +1,155 @@
+# x = 10
+# y = 20
+
+# print(f'x == y {x == y}')
+# print(f'x != y {x != y}')
+# print(f'x > y {x > y}')
+# print(f'x < y {x < y}')
+# print(f'x >= y {x >= y}')
+# print(f'x <= y {x <= y}')
+
+# x = 15
+# y = 15
+# print(f'x >= y {x >= y}')
+# print(f'x <= y {x <= y}')
+
+# # 논리연산자
+# print(True and True)
+# print(True and False)
+# print(False and True)
+# print(False and False)
+
+# print(True or True)
+# print(True or False)
+# print(False or True)
+# print(False or False)
+
+# print(f'not True : {not True}')
+# print(f'not False : {not False}')
+
+# print(True and False or True)
+# print(True and (False or True) and False)
+
+# # a = False
+# # print(a=5 and True)
+# # print(f'a : {a}')
+
+# a = 10
+# if a == 10:
+#     print(f'a: {a}')
+#     print('if문 블럭 안')
+
+# print('if문 블럭 밖')
+
+# age = 20
+# if age >= 18:
+#     print("성인입니다.")
+
+# name = "hotdog"
+# if name:
+#     print("이딴것도 이름입니까?")
+
+# name = ""
+# if name:
+#     print("당신에겐 이름도 없습니까?")
+
+# if True:
+#     pass
+# print("조건문관 상관없습니다.")
+
+# weather = input("오늘의 날씨를 입력해 주세요 : ")
+# rain = "비"
+# clear = "맑음"
+
+# if weather == rain:
+#     print("우산을 챙기세요!")
+# else:
+#     print("선크림을 듬뿍 바르세요!")
+
+
+# age = int(input("나이를 입력하세요 : "))
+
+# if age >= 18:
+#     print("성인입니다.")
+# else:
+#     print("미성년자입니다.")
+
+
+# programe = int(input("정수를 입력해 주세요 : "))
+# x = programe
+# if x % 2 == 0:
+#     print("짝수입니다.")
+# else:
+#     print("홀수입니다.")
+
+# name = "안철수"
+
+# if name == "안철수":
+#     print(f"안쳤습니다.")
+# elif name == "철수":
+#     print(f"철수입니다.")
+# else:
+#     print("아닙니다.")
+
+
+# 나이에 따른 영화 관란 가능 여부
+# age0 = int(input("나이를 입력하세요 : "))
+# age1 = [0, 12]
+# age2 = [13, 15]
+# age3 = [16, 18]
+# age4 = [19, 100]
+
+# if age0 <= 13:
+#     print("전체 관람가")
+# elif age0 <= 16:
+#     print("12세 이상 관람가")
+# elif age0 <= 19:
+#     print("15세 이상 관람가")
+# else:
+#     print("청소년 관람불가 가능")
+
+# time = int(input("초를 입력하세요 : "))
+
+# if time <= 20:
+#     print("20초")
+# elif time <= 90:
+#     print("1분 30초")
+# else:
+#     print("1시간 16분 7초")
+
+# second = int(input("초를 입력해주세요 : "))
+
+# if second >= 3600:
+#     print(f'{second // 3600} 시', end=' ')
+
+# second %= 3600
+
+# if second >= 60:
+#     print(f'{second // 60} 분', end=' ')
+
+# else:
+#     print(f'{second // 60} 초')
+
+
+# if True
+#     if True:
+#         if True:
+#             pass
+#         if True:
+#             pass
+#         if True:
+#             pass
+
+
+money = int(input("돈을 지불해라 : "))
+food = input("상품을 선택하여주십시오 김밥, 삼각김밥, 도시락 : ")
+kimbab = 2500
+samgak = 1500
+dosirak = 4000
+
+if money == food:
+    # food = input("김밥 삼각김밥 도시락").split
+    if money <= 2500:
+        print("삼각김밥 대령이요.")
+        if money > 2500:
+            print("돈이 맞지 않습니다!")
